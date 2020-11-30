f1afd7b22573e0c52c335c800652ef41c138c47b
Aric Akbani <aricakbani@gmail.com>
2020-11-29 22:52:33 -0600

    updated seive

--- Seive_of_Eratosthenes.py
+++ Seive_of_Eratosthenes.py
@@ -1,18 +1,16 @@
-# Finds all prime numbers in a given range
-
 print('The Seive of Eratosthenes will find all prime numbers in a given range')
 
-def seive_of_e():
-  limit = input('Input an integer for the upper limit: ')
+def seive_of_e(limit):
   limit = int(limit) + 1
 
   sieve = [True] * (limit)
-  output = []
+  output = [1]
   for i in range(2, int(limit)):
     if sieve[i]:
-      output += [str(i)]
+      output += [i]
       for j in range(i*i, int(limit), i):
         sieve[j] = False
   print(f'Prime numbers: {output}')
 
-seive_of_e()
+limit = input('Input an integer for the upper limit: ')
+seive_of_e(limit)
